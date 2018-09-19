32fe54a8b3e1ced47b7a3fb35339bcf8f674cd7c
Glave <36699022+Glave@users.noreply.github.com>
2018-09-19 20:45:44 +0800

    Add project files.

---
+++ KPMS/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+namespace KPMS.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AnotherLink()
+        {
+            return View("Index");
+        }
+        public ActionResult SamplePage()
+        {
+            return View();
+        }
+    }
+}
